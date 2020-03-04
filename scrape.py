3e646dd44d92be19981307df5665f1327d4ba7d1
DK the Human <dk@dkthehuman.com>
2020-03-03 21:04:30 -0500

    update scraper for new api

--- scrape.py
+++ scrape.py
@@ -4,37 +4,40 @@ import requests
 
 
 seen_photos = {}
+url = 'https://earthview.withgoogle.com/_api/polanczyk-poland-5484.json'
+count = 0
 
 
-def recursive_scrape(url, count=0):
-
+def scrape():
     # The API seems to link the images in a loop, so we can stop once we see an
     # image we have already seen.
-    if url in seen_photos:
-        return
+    global url
     seen_photos[url] = True
 
     page = requests.get(url)
     photo_json = page.json()
 
-    print photo_json
-    yield photo_json
+    photo = {
+        "id": photo_json['id'],
+        "earthLink": photo_json['earthLink'],
+    }
+    if 'country' in photo_json:
+        photo['country'] = photo_json['country']
+    if 'region' in photo_json:
+        photo['region'] = photo_json['region']
 
-    next_url = 'https://earthview.withgoogle.com' + photo_json['nextApi']
-
-    # Yielding from recursive functions is a bit funky
-    for photo_json in recursive_scrape(next_url, count + 1):
-        yield photo_json
+    url = 'https://earthview.withgoogle.com/_api/' + \
+        photo_json['nextSlug'] + '.json'
+    return photo
 
 
 if __name__ == "__main__":
-    # Google Earth View contains around 1500 photos, so we need to up the
-    # recursion limit
-    sys.setrecursionlimit(2000)
-
-    photos_json = json.dumps(list(recursive_scrape(
-        'https://earthview.withgoogle.com/_api/polanczyk-poland-5484.json'
-    )))
-
     with open('earthview.json', 'w+') as f:
-        f.write(photos_json)
+        while True:
+            if url in seen_photos:
+                break
+            photo_json = json.dumps(scrape())
+            print(count)
+            print(photo_json)
+            f.write(photo_json)
+            count += 1
